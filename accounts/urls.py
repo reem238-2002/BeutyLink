177a4e6f588e0df55953c80ac3d028f9abd5a4d3
reem <reemabdallh809@gmail.com>
2026-03-31 20:10:55 +0300

    "edit login and signup"

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('salon-dashboard/', views.salon_dashboard, name='salon_dashboard'),
     path('client-dashboard/', views.client_dashboard, name='client_dashboard'),
-
 ]
